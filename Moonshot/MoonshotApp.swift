b88543d998d6523eab0c3dfd68cc5c096419177d
d.o.kononov <d.o.kononov@gmail.com>
2025-09-02 10:51:47 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dmitry Kononov on 2.09.25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
